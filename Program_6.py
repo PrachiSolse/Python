b8091b1ac47f9d564799a17ca2232f4a08488554
PrachiSolse <prachi18st@gmail.com>
2026-01-15 20:06:59 +0530

    Greater number

---
+++ Program_6.py
@@ -0,0 +1,8 @@
+#Print the largest of two numbers.
+num1=int(input("Enter 1st number: "))
+num2=int(input("Enter 2nd number: "))
+if(num1>num2):
+    print(num1,"is greater.")
+else:
+    print(num2,"is greater.")
+    
