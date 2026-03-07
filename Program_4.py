2983a500056d155517e7b4b4daa990d8e691cf98
PrachiSolse <prachi18st@gmail.com>
2026-03-07 21:52:15 +0530

    Swap variable

---
+++ Program_4.py
@@ -0,0 +1,9 @@
+# Swap two variable
+a=int(input("Enter 1st Number: "))
+b=int(input("Enter 2nd Number: "))
+print("Numbers before swap",a ,"and",b)
+c=a
+a=b
+b=c
+print("Number after swap",a, "and",b)
+
