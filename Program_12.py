e347c5b9ee9e56555899446bf87f625acb93fc65
PrachiSolse <prachi18st@gmail.com>
2026-03-15 14:43:07 +0530

    Second small and large

--- Program_12.py
+++ Program_12.py
@@ -1,4 +1,4 @@
 #Second smallest and second largest
-arr=list(map(int,input("Enter the Array:").split()))
+arr=list(set(map(int,input("Enter the Array:").split())))
 arr.sort()
 print("Second smallest is",arr[1],"and second largest is",arr[-2])
